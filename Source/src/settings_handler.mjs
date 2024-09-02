f00f4d45b31b644fc622c4f0bea9ce4c46cefe54
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2024-09-02 20:59:39 +0200

    Added connection settings and fixed AP server connect

---
+++ Source/src/settings_handler.mjs
@@ -0,0 +1,71 @@
+import { ITEMS_HANDLING_FLAGS } from "https://unpkg.com/archipelago.js@1.0.0/dist/archipelago.js";
+
+// Set up the connection information.
+export const connectionInfo = {
+  hostname: "archipelago.gg", // Replace with the actual AP server hostname.
+  port: 0, // Replace with the actual AP server port.
+  game: "The Wind Waker", // Replace with the game name for this player.
+  name: "Player1", // Replace with the player slot name.
+  password: "",
+  version: {
+    major: 0, 
+    minor: 5, 
+    build: 0
+  },
+  items_handling: ITEMS_HANDLING_FLAGS.REMOTE_ALL
+};
+
+export function setupSettings(ctx){
+    const apConnection = ctx.settings.section('Connection');
+
+    apConnection.add({
+      type: 'text',
+      name: 'ap-hostname',
+      label: 'Host name',
+      hint: 'Host name of the AP world, for example archipelago.gg',
+      default: "archipelago.gg"
+    });
+
+    apConnection.add({
+      type: 'number',
+      name: 'ap-port',
+      label: 'Port',
+      hint: 'Port of the AP world.',
+      default: 0
+    });
+  
+    apConnection.add({
+      type: 'text',
+      name: 'ap-slotname',
+      label: 'Slot name',
+      hint: 'Slot name of the player, for example Player1',
+      default: "Player1"
+    });
+  
+    apConnection.add({
+      type: 'text',
+      name: 'ap-password',
+      label: 'Password',
+      hint: 'Password of the AP room',
+      default: ""
+    });
+  
+    const sidebarApStatus = sidebar.category('AP', {
+      before: "Combat",
+      ignoreToggle: true 
+    }, (greetings) => 
+      {
+      greetings.item('Not Connected');
+    });
+}
+
+export function setConnectionInfo(ctx){
+  const apConnectionInfo = ctx.settings.section('Connection')
+
+  connectionInfo.hostname = apConnectionInfo.get('ap-hostname');
+  connectionInfo.port = Number(apConnectionInfo.get('ap-port'));
+  connectionInfo.password = apConnectionInfo.get('ap-password');
+  connectionInfo.name = apConnectionInfo.get('ap-slotname')
+
+  console.log(connectionInfo);
+}
--- Source/src/setup.mjs
+++ Source/src/setup.mjs
@@ -1,14 +1,33 @@
-export function setup(ctx) {
-  ctx.settings.section('General').add({
-    type: 'number',
-    name: 'xp-multiplier',
-    label: 'XP Multiplier',
-    hint: 'Multiply all XP gains by this amount',
-    default: 1
-  });
+// ES module import, but you can use CommonJS syntax as well, if you prefer.
+import { Client, ITEMS_HANDLING_FLAGS } from "https://unpkg.com/archipelago.js@1.0.0/dist/archipelago.js";
 
-  ctx.patch(Skill, 'addXP').before(function(amount, masteryAction) {
-    const xpMultiplier = ctx.settings.section('General').get('xp-multiplier');
-    return [amount * xpMultiplier, masteryAction];
+// Create a new instance of the Client class.
+const client = new Client();
+
+let settingsHandler = null;
+
+export async function setup(ctx) {
+  settingsHandler = await ctx.loadModule('src/settings_handler.mjs');
+  settingsHandler.setupSettings(ctx);
+
+  ctx.onInterfaceReady(async ctx => {
+    settingsHandler.setConnectionInfo(ctx);
+    ConnectToAP(settingsHandler.connectionInfo);
+  });
+}
+
+async function ConnectToAP(connectionInfo){
+  console.log("Trying to connect to " + connectionInfo.hostname + ":" + String(connectionInfo.port));
+
+  client
+  .connect(connectionInfo)
+  .then(() => {
+    console.log("Connected to the server");
+    client.say("Howdy");
+    // You are now connected and authenticated to the server. You can add more code here if need be.
+  })
+  .catch((error) => {
+    console.error("Failed to connect:", error);
+    // Handle the connection error.
   });
 }
