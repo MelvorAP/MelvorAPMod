1ec48e97ecaacd7c2429ad65656ccb257dbabd30
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2024-09-03 18:22:26 +0200

    Added more settings in slot data and improved how said stats are set

--- Source/src/stats_handler.mjs
+++ Source/src/stats_handler.mjs
@@ -1,14 +1,51 @@
 
 export const stats = {
-    exp_gain: 1,
-    mastery_gain: 1
+    expGain: 0,
+    masteryGain: 0,
+    currencyGain: 0,
+    slayerCoinGain: 0,
+    maxMasteryPoolCap: 0,
+    bankSpaceIncrease: 0
   };
 
 export function setSlotData(ctx, slotData){
-    stats.exp_gain = slotData.exp_gain
-    stats.mastery_gain = slotData.mastery_gain
+    stats.expGain = slotData.exp_gain;
+    stats.masteryGain = slotData.mastery_gain;
+    
+    stats.currencyGain = slotData.currency_gain;
+    stats.slayerCoinGain = slotData.slayer_gain;
+    
+    stats.maxMasteryPoolCap = slotData.max_mastery_pool_cap;
+    stats.bankSpaceIncrease = slotData.bank_space_increase;
 
-    ctx.patch(Skill, 'addXP').before(function(amount, masteryAction) {
-        return [amount * stats.exp_gain, masteryAction];
-      });
+    setModifier("melvorD:skillXP", stats.expGain);
+    setModifier("melvorD:masteryXP", stats.masteryGain);
+    
+    setModifier("melvorD:currencyGain", stats.currencyGain);
+    setModifier("melvorD:currencyGainFromSlayerTasks", stats.slayerCoinGain);
+    
+    setModifier("melvorD:masteryPoolCap", stats.maxMasteryPoolCap);
+    setModifier("melvorD:bankSpace", stats.bankSpaceIncrease);
+}
+
+function setModifier(skill, gain){
+  if(gain == 0){
+    return;
+  }
+
+  const modifierValue = new ModifierValue(game.modifierRegistry.getObjectByID(skill), gain, "archipelago");
+  
+  // Create a stat provider with the modifier bonuses in it
+  const statObject = {
+    modifiers: [modifierValue]
+  };
+  
+  const statProvider = new StatProvider();
+  statProvider.addStatObject({ name: "Archipelago boost" }, statObject);
+  
+  // Register the stat provider on the combat manager
+  game.combat.registerStatProvider(statProvider);
+  
+  // Ensure the stat calculations and UI are up-to-date
+  game.combat.computeAllStats();
 }
